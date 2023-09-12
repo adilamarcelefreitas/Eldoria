dae2f84e082acc7acc0aeec704d2bbf4d61131c3
Ádila Freitas <adilamarcele@hotmail.com>
2023-09-12 17:33:36 -0300

    Implementação Firebase

---
+++ src/firebase/firebaseinit.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6om3vBnULRaPmBQlLGONO0PXPjUank-w",
+  authDomain: "eldoria-11678.firebaseapp.com",
+  projectId: "eldoria-11678",
+  storageBucket: "eldoria-11678.appspot.com",
+  messagingSenderId: "640702730400",
+  appId: "1:640702730400:web:ff51a859fec8f7c8eab9c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
